5deed81a6f85e83ad9465158c1c8d74ac12b53ec
godmonth <godmonth@gmail.com>
2015-11-20 09:55:08 +0800

    commit

--- src/main/java/com/godmonth/sfexpress/bsp/protocol/ordersearch/OrderSearchResponse.java
+++ src/main/java/com/godmonth/sfexpress/bsp/protocol/ordersearch/OrderSearchResponse.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import com.godmonth.sfexpress.bsp.protocol.Response;
+import com.godmonth.sfexpress.bsp.protocol.ResponseError;
 import com.godmonth.sfexpress.bsp.protocol.order.OrderResponseBody;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
@@ -12,6 +13,19 @@ public class OrderSearchResponse extends Response {
 
 	@XStreamAlias("Body")
 	private OrderResponseBody body;
+
+	public OrderSearchResponse() {
+	}
+
+	public OrderSearchResponse(String serviceName, String head, OrderResponseBody body) {
+		setService(serviceName);
+		setHead(head);
+		this.body = body;
+	}
+
+	public OrderSearchResponse(String serviceName, String head, ResponseError error) {
+		super(serviceName, head, error);
+	}
 
 	public OrderResponseBody getBody() {
 		return body;
@@ -25,9 +39,8 @@ public class OrderSearchResponse extends Response {
 	 * @see java.lang.Object#toString()
 	 */
 	public String toString() {
-		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-				.appendSuper(super.toString()).append("body", this.body)
-				.toString();
+		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).appendSuper(super.toString())
+				.append("body", this.body).toString();
 	}
 
 }
